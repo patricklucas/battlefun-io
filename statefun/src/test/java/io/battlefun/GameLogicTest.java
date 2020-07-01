ef78f81755a43e11e94dc46a89eb447af43695f9
Igal Shilman <igalshilman@gmail.com>
2020-07-01 12:14:28 +0200

    Add game logic tests

--- statefun/src/test/java/io/battlefun/GameLogicTest.java
+++ statefun/src/test/java/io/battlefun/GameLogicTest.java
@@ -18,14 +18,19 @@
 
 package io.battlefun;
 
+import io.battlefun.generated.FromGameFn.Failure;
 import io.battlefun.generated.FromGameFn.GameUpdate;
 import io.battlefun.generated.GameStatus;
 import io.battlefun.generated.Ship;
 import io.battlefun.generated.ShipPlacement;
 import io.battlefun.generated.ShipPlacement.Builder;
+import io.battlefun.generated.Shot;
 import io.battlefun.generated.ToGameFn.CreateGame;
+import io.battlefun.generated.ToGameFn.Turn;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -38,8 +43,8 @@ public class GameLogicTest {
             .setGameId("game-1")
             .setPlayer1Id("player-1")
             .setPlayer2Id("player-2")
-            .setPlayer1Placement(from("1 2 3", "5 6 7", "8 9 10 11"))
-            .setPlayer2Placement(from("1 2 3", "5 6 7", "8 9 10 11"))
+            .setPlayer1Placement(ships("1 2 3", "5 6 7", "8 9 10 11"))
+            .setPlayer2Placement(ships("1 2 3", "5 6 7", "8 9 10 11"))
             .build();
 
     GameUpdate created = GameLogic.create(create);
@@ -48,9 +53,111 @@ public class GameLogicTest {
     assertThat(created.getGameId(), is("game-1"));
     assertThat(created.getPlayer1Id(), is("player-1"));
     assertThat(created.getPlayer2Id(), is("player-2"));
+    assertThat(created.getPlayer1Placement(), CoreMatchers.notNullValue());
+    assertThat(created.getPlayer2Placement(), CoreMatchers.notNullValue());
   }
 
-  private static ShipPlacement from(String pos1, String pos2, String pos3) {
+  @Test
+  public void firstTurnGoesToPlayer1() {
+    GameUpdate game =
+        game(ships("1 2 3", "5 6 7", "8 9 10 11"), ships("1 2 3", "5 6 7", "8 9 10 11"));
+
+    assertThat(game.getStatus(), is(GameStatus.PLAYER1_TURN));
+  }
+
+  @Test
+  public void alternateTurns() {
+    GameUpdate game =
+        game(ships("1 2 3", "5 6 7", "8 9 10 11"), ships("1 2 3", "5 6 7", "8 9 10 11"));
+
+    for (int i = 0; i < 100; i++) {
+      game = applyTurn(game, "player-1", 999999 + i);
+      assertThat(game.getStatus(), is(GameStatus.PLAYER2_TURN));
+      game = applyTurn(game, "player-2", 999999 + i);
+      assertThat(game.getStatus(), is(GameStatus.PLAYER1_TURN));
+    }
+  }
+
+  @Test
+  public void guessIsKeptInHistory() {
+    GameUpdate game =
+        game(ships("1 2 3", "5 6 7", "8 9 10 11"), ships("1 2 3", "5 6 7", "8 9 10 11"));
+
+    game = applyTurn(game, "player-1", 50);
+
+    assertThat(game.getPlayer1ShotsList(), hasItem(shot(50)));
+  }
+
+  @Test
+  public void player1Wins() {
+    GameUpdate game =
+        game(ships("1 2 3", "4 5 6 7", "8 9 10 11"), ships("1 2 3", "4 5 6 7", "8 9 10 11"));
+
+    //
+    // 1. destroy all the cells of player-2 except cell #11
+    //
+    for (int i = 1; i <= 10; i++) {
+      game = applyTurn(game, "player-1", i); // hits
+      game = applyTurn(game, "player-2", 1_000_00 + i); // misses
+    }
+    //
+    // 2. kill player's 2 last remaining cell
+    //
+    game = applyTurn(game, "player-1", 11);
+
+    assertThat(game.getStatus(), is(GameStatus.PLAYER1_WIN));
+  }
+
+  @Test
+  public void player2Wins() {
+    GameUpdate game =
+        game(ships("1 2 3", "4 5 6 7", "8 9 10 11"), ships("1 2 3", "4 5 6 7", "8 9 10 11"));
+
+    //
+    // 1. destroy all the cells of player-2 except cell #11
+    //
+    for (int i = 1; i <= 10; i++) {
+      game = applyTurn(game, "player-1", 1_000_00 + i); // misses
+      game = applyTurn(game, "player-2", i); // hits
+    }
+    //
+    // 2. since it is player's 1 turn, make another dummy miss.
+    //
+    game = applyTurn(game, "player-1", 5_000_00); // misses
+    //
+    // 3. kill player's 1 last remaining cell
+    //
+    game = applyTurn(game, "player-2", 11);
+
+    assertThat(game.getStatus(), is(GameStatus.PLAYER2_WIN));
+  }
+
+  private static Shot shot(int cell) {
+    return Shot.newBuilder().setCellId(cell).build();
+  }
+
+  private static GameUpdate applyTurn(GameUpdate game, String playerId, int guess) {
+    Turn turn = Turn.newBuilder().setGameId("game-1").setPlayerId(playerId).setShot(guess).build();
+    Either<GameUpdate, Failure> next = GameLogic.apply(game, turn);
+    assertThat(next.isLeft(), is(true));
+    game = next.left;
+    return game;
+  }
+
+  private static GameUpdate game(ShipPlacement p1, ShipPlacement p2) {
+    CreateGame create =
+        CreateGame.newBuilder()
+            .setGameId("game-1")
+            .setPlayer1Id("player-1")
+            .setPlayer2Id("player-2")
+            .setPlayer1Placement(p1)
+            .setPlayer2Placement(p2)
+            .build();
+
+    return GameLogic.create(create);
+  }
+
+  private static ShipPlacement ships(String pos1, String pos2, String pos3) {
     Builder builder = ShipPlacement.newBuilder();
     addPositions(builder.addShipsBuilder(), "a", pos1);
     addPositions(builder.addShipsBuilder(), "b", pos2);
